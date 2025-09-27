6f7bb4d0a18b9336dc7ee67b7cdf3acab69f41a2
ProgrammerGLQ <guillermo.quinteros073@gmail.com>
2025-09-27 11:53:57 -0300

    fix errors

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
       description: 'Users Api'
     },
     host: 'localhost:3001',
-    schemes: [ 'https', 'http']
+    schemes: [ 'https']
   };
 
 const outputFile = './swagger.json';
