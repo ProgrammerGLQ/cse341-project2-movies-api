66d4177dc78a0c83af25f4d239c515a1eb69903f
ProgrammerGLQ <guillermo.quinteros073@gmail.com>
2025-10-07 19:39:04 -0300

    W04 Project: Project 2 Part 2 (Authentication)

--- routes/directors.js
+++ routes/directors.js
@@ -1,24 +1,13 @@
-const express = require('express');
-const router = express.Router();
-
+const router = require('express').Router();
 const directorController = require('../controllers/directors'); 
 const validation = require('../middleware/validate'); 
+const isAuthenticated = require('../middleware/authenticate').isAuthenticated; 
 
-
-// 1. GET All
 router.get('/', directorController.getAll);
-
-// 2. GET Single
 router.get('/:id', directorController.getSingle);
 
-// 3. POST ( VALIDATION)
-router.post('/', validation.savedirector, directorController.createdirector); 
-
-// 4. PUT ( VALIDATION)
-router.put('/:id', validation.savedirector, directorController.updatedirector);
-
-// 5. DELETE
-router.delete('/:id', directorController.deletedirector);
-
+router.post('/', isAuthenticated, validation.savedirector, directorController.createdirector); 
+router.put('/:id', isAuthenticated, validation.savedirector, directorController.updatedirector);
+router.delete('/:id', isAuthenticated, directorController.deletedirector);
 
 module.exports = router;
--- routes/movies.js
+++ routes/movies.js
@@ -4,12 +4,15 @@ const router = express.Router();
 const moviesController = require('../controllers/movies');
 const validation = require('../middleware/validate');
 
+const isAuthenticated = require('../middleware/authenticate').isAuthenticated; 
+
+
 router.get('/', moviesController.getAll);
 router.get('/:id', moviesController.getSingle);
 
-router.post('/', validation.savemovie, moviesController.createmovie);
-router.put('/:id', validation.savemovie, moviesController.updatemovie);
 
-router.delete('/:id', moviesController.deletemovie);
+router.post('/', isAuthenticated, validation.savemovie, moviesController.createmovie);
+router.put('/:id', isAuthenticated, validation.savemovie, moviesController.updatemovie);
+router.delete('/:id', isAuthenticated, moviesController.deletemovie);
 
 module.exports = router;
